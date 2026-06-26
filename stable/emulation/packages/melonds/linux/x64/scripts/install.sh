a8c74f7a81b7bcff27288d394cecdf5f7d18d5b6
nxtroox <100068674+nxtroox@users.noreply.github.com>
2026-06-26 22:48:29 +0200

    Fix melonDS install script

--- stable/emulation/packages/melonds/linux/x64/scripts/install.sh
+++ stable/emulation/packages/melonds/linux/x64/scripts/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MELONDS_URL="https://github.com/melonDS-emu/melonDS/releases/download/1.1/melonDS-1.1-ubuntu-x86_64.zip"
 
-curl -o /tmp/melonds.zip $MELONDS_URL
+curl -L -o /tmp/melonds.zip $MELONDS_URL
 
-unzip /tmp/melonds.zip -o /usr/bin
+unzip /tmp/melonds.zip -d /usr/bin
 
 rm /tmp/melonds.zip
